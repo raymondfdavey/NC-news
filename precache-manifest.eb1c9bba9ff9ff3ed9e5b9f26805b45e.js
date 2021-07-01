30722c03bf410f87fe6a4ae8b64cd6949f678de1
rfd <rfd@rfds-MacBook-Air.local>
2021-07-01 23:05:24 +0100

    Updates

---
+++ precache-manifest.eb1c9bba9ff9ff3ed9e5b9f26805b45e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d488cf7786896e0e1bbd3cc04dc111aa",
+    "url": "/NC-news/index.html"
+  },
+  {
+    "revision": "5e3de6f80fd644a10165",
+    "url": "/NC-news/static/css/main.af2e897d.chunk.css"
+  },
+  {
+    "revision": "4942ce827e3666f1e2eb",
+    "url": "/NC-news/static/js/2.9364b481.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/NC-news/static/js/2.9364b481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3de6f80fd644a10165",
+    "url": "/NC-news/static/js/main.86af6182.chunk.js"
+  },
+  {
+    "revision": "c50b6a7d046dafa84573",
+    "url": "/NC-news/static/js/runtime-main.e33fbdd5.js"
+  }
+]);
